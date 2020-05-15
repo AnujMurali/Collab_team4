aceef510ac1bc573ed3054c21789c8e98f17a162
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-15 17:14:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include "collab.h"
 
 main() {
-  a = 10;
-  b = 5;
+  int a = 10;
+  int b = 5;
   printf ("Add: %d",add(a,b));
   printf ("Subtract: %d",sub(a,b));
-  printf ("Multiply: %d",multiply(a,b);
+  printf ("Multiply: %d",multiply(a,b));
   printf ("Divide: %d",divide(a,b));
  }
