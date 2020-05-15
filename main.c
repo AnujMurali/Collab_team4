309c316bbdac962d69392065c9477656d5da6a78
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-15 17:12:12 -0400

    Update main.c
    added use of individual functions

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include "collab.h"
 
 main() {
-
-  printf ("Hello World");
-  
+  a = 10;
+  b = 5;
+  printf ("Add: %d",add(a,b));
+  printf ("Subtract: %d",sub(a,b));
+  printf ("Multiply: %d",multiply(a,b);
+  printf ("Divide: %d",divide(a,b));
  }
